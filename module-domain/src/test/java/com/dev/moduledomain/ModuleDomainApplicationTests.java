4407efee529d4043e1233b04dea86b9d2f32f278
hehekim <tytntu@gmail.com>
2023-03-18 20:09:00 +0900

    feat : 멀티모듈 기본 셋팅하기
    - module-api : 블로그검색, 인기키워드 목록조회를 요청받고 비즈니스 로직을 처리하는 모듈
    - module-client : 외부 블로그검색 api를 연동시킬 모듈
    - module-domain : 인기키워드 도메인에 관련 된 entity, repository가 있는 모듈

---
+++ module-domain/src/test/java/com/dev/moduledomain/ModuleDomainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.moduledomain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleDomainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
