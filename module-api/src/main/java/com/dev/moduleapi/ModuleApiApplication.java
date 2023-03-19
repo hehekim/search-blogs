8a87ca2fa6e44835045ac3fb1750fbc10623848a
hehekim <tytntu@gmail.com>
2023-03-19 10:08:56 +0900

    chore : client application 삭제 및 api application basePackage 적용

--- module-api/src/main/java/com/dev/moduleapi/ModuleApiApplication.java
+++ module-api/src/main/java/com/dev/moduleapi/ModuleApiApplication.java
@@ -3,7 +3,11 @@ package com.dev.moduleapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = { "com.dev.moduleapi", "com.dev.moduleclient" }
+)
+//@EntityScan("com.dev.moduledomain.domain")
+//@EnableJpaRepositories(basePackages = "dev.be.modulecommon.repositories")
 public class ModuleApiApplication {
 
     public static void main(String[] args) {
