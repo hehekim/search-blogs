c1791731da8b0d665293644761a001fe1e708a4c
hehekim <tytntu@gmail.com>
2023-03-19 11:44:36 +0900

    feat : entity와 repository를 module-api에서 사용할 수 있도록 설정

--- module-api/src/main/java/com/dev/moduleapi/ModuleApiApplication.java
+++ module-api/src/main/java/com/dev/moduleapi/ModuleApiApplication.java
@@ -2,12 +2,14 @@ package com.dev.moduleapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(
         scanBasePackages = { "com.dev.moduleapi", "com.dev.moduleclient" }
 )
-//@EntityScan("com.dev.moduledomain.domain")
-//@EnableJpaRepositories(basePackages = "dev.be.modulecommon.repositories")
+@EntityScan("com.dev.moduledomain.entity")
+@EnableJpaRepositories(basePackages = "com.dev.moduledomain.repository")
 public class ModuleApiApplication {
 
     public static void main(String[] args) {
