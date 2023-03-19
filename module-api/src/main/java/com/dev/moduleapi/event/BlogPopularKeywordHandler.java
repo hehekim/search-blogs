2bafaa7923f30a5b5e24ec6a9e73c405d18b33e6
hehekim <tytntu@gmail.com>
2023-03-19 16:46:48 +0900

    fix : @TransactionalEventListener로 인해 인기키워드 생성되지 않는 버그 수정
    - 해결방안 : @TransactionalEventListener는 기존에 트랜잭션이 있어야 실행이 된다.
    - 이벤트리스너를 적용하기 위해 @EventListener 로 대체

--- module-api/src/main/java/com/dev/moduleapi/event/BlogPopularKeywordHandler.java
+++ module-api/src/main/java/com/dev/moduleapi/event/BlogPopularKeywordHandler.java
@@ -2,18 +2,18 @@ package com.dev.moduleapi.event;
 
 import com.dev.moduleapi.service.BlogPopularKeywordService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.event.TransactionalEventListener;
+import org.springframework.transaction.annotation.Transactional;
 
-@Slf4j
 @Component
 @RequiredArgsConstructor
 public class BlogPopularKeywordHandler {
     private final BlogPopularKeywordService popularKeywordService;
 
-    @TransactionalEventListener
+    @EventListener
+    @Transactional
     @Async
     public void saveBlogPopularKeyword(BlogPopularKeywordEvent event){
         popularKeywordService.saveBlogPopularKeyword(event.getKeyword());
