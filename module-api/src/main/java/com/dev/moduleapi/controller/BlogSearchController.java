bea9a493cd79b7c1709970b7457339fa2b1e1d4c
hehekim <tytntu@gmail.com>
2023-03-20 09:02:42 +0900

    feat : 블로그 검색 컨트롤러 http method 변경
    - 검색기능만 들어있을 때 http method를 GET으로 설정해주었지만, 인기키워드 저장 로직이 추가되어 POST로 변경

---
+++ module-api/src/main/java/com/dev/moduleapi/controller/BlogSearchController.java
@@ -0,0 +1,23 @@
+package com.dev.moduleapi.controller;
+
+import com.dev.moduleapi.dto.request.BlogSearchRequest;
+import com.dev.moduleapi.dto.response.Response;
+import com.dev.moduleapi.dto.response.BlogSearchResponse;
+import com.dev.moduleapi.service.BlogSearchService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/blogs")
+public class BlogSearchController {
+
+    private final BlogSearchService blogService;
+
+    @PostMapping
+    public Response<BlogSearchResponse> searchBlogs(@RequestBody @Valid BlogSearchRequest body) {
+        return Response.success(blogService.searchBlogsByKeyword(body));
+    }
+}
