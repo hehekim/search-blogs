029cad32ffc40fcbbf2f758e4264b45a730205d2
hehekim <tytntu@gmail.com>
2023-03-20 21:43:15 +0900

    fix : yml 분리로 인해 client 모듈에서 버그 발생하는 것 해결
    - 원인 : test에서 yml을 읽지 못해 빌드 실패
    - @SpringBootTest에 읽어야 하는 yml 이름을 적용
    - api 모듈 실행할 때 yml 읽지 못하는 것 해결 (application에 System.setProperty()를 적용)

--- module-api/src/test/java/com/dev/moduleapi/ModuleApiApplicationTests.java
+++ module-api/src/test/java/com/dev/moduleapi/ModuleApiApplicationTests.java
@@ -3,7 +3,7 @@ package com.dev.moduleapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {"spring.config.location=classpath:application-client.yml"})
 class ModuleApiApplicationTests {
 
     @Test
--- module-client/src/main/java/com/dev/moduleclient/ModuleClientApplication.java
+++ module-client/src/main/java/com/dev/moduleclient/ModuleClientApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ModuleClientApplication {
 
     public static void main(String[] args) {
+        System.setProperty("spring.config.name", "application-client");
         SpringApplication.run(ModuleClientApplication.class, args);
     }
 
