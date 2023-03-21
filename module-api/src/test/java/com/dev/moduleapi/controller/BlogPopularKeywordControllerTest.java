e4d2087d20e4fcf4c97cc5db8012743d0e2eeefe
hehekim <tytntu@gmail.com>
2023-03-22 03:10:32 +0900

    test : 인기키워드 컨트롤러 테스트 BDD mockito로 변경

--- module-api/src/test/java/com/dev/moduleapi/controller/BlogPopularKeywordControllerTest.java
+++ module-api/src/test/java/com/dev/moduleapi/controller/BlogPopularKeywordControllerTest.java
@@ -1,8 +1,8 @@
 package com.dev.moduleapi.controller;
 
-import com.dev.moduleapi.fixture.BlogPopularKeywordResponseFixture;
-import com.dev.moduleapi.exception.SearchApplicationException;
 import com.dev.moduleapi.exception.ErrorCode;
+import com.dev.moduleapi.exception.SearchApplicationException;
+import com.dev.moduleapi.fixture.BlogPopularKeywordResponseFixture;
 import com.dev.moduleapi.service.BlogPopularKeywordApiService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,8 +12,8 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.when;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -30,27 +30,28 @@ class BlogPopularKeywordControllerTest {
     @Test
     void getTenPopularKeywords() throws Exception {
         // Given
-        when(popularKeywordService.getTenPopularKeywords())
-                .thenReturn(BlogPopularKeywordResponseFixture.createTop10Keywords());
+        given(popularKeywordService.getTenPopularKeywords())
+                .willReturn(BlogPopularKeywordResponseFixture.createTop10Keywords());
 
         // When & Then
         mvc.perform(get("/popular-keywords")
                     .contentType(MediaType.APPLICATION_JSON)
                 ).andDo(print())
                 .andExpect(status().isOk());
+        then(popularKeywordService).should().getTenPopularKeywords();
     }
 
     @DisplayName("[GET] 인기키워드(검색 횟수가 많은 기준) 상위 10개 조회 시 조회 데이터가 없을 때 에러발생")
     @Test
     void findTenPopularKeywordsReturnNoData() throws Exception {
         // Given
-        doThrow(new SearchApplicationException(ErrorCode.POPULAR_KEYWORD_NOT_FOUND))
-                .when(popularKeywordService).getTenPopularKeywords();
+        given(popularKeywordService.getTenPopularKeywords()).willThrow(new SearchApplicationException(ErrorCode.POPULAR_KEYWORD_NOT_FOUND));
 
         // When & Then
         mvc.perform(get("/popular-keywords")
                         .contentType(MediaType.APPLICATION_JSON)
                 ).andDo(print())
                 .andExpect(status().isNotFound());
+        then(popularKeywordService).should().getTenPopularKeywords();
     }
 }
